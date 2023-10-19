4ca1e5a3a09fe42bb367b8e2f46b366d85869781
Ray Valenzuela <rvalen14@calpoly.edu>
2023-10-18 21:18:52 -0700

    Finished with typescript hopefully

--- AceOfRaysPersonalWebsite/scripts/blog.js
+++ AceOfRaysPersonalWebsite/scripts/blog.js
@@ -1,11 +1,4 @@
-type Blog = {
-    title: string,
-    date: string,
-    description: string,
-    slug: string
-}
-
-const myList: Blog[] = [
+var myList = [
     {
         title: "First time using TypeScript!",
         date: "October 18th, 2023",
@@ -18,48 +11,37 @@ const myList: Blog[] = [
         description: "At least my lovely GF is here with me.",
         slug: "blog.html"
     }
-]
-
-function addBlogToDOM(blogList: Blog[]): void {
-    const container = document.getElementById('postContainer');
-
+];
+function addBlogToDOM(blogList) {
+    var container = document.getElementById('postContainer');
     if (container) {
-        blogList.forEach(blog => {
-            const finalDiv = document.createElement('div');
-            const titleDiv = document.createElement('div');
-            const secondDiv = document.createElement('div');
-            const title = document.createElement('h1');
-            const date = document.createElement('h4');
-            const text = document.createElement('p');
-            const link = document.createElement('a');
-
+        blogList.forEach(function (blog) {
+            var finalDiv = document.createElement('div');
+            var titleDiv = document.createElement('div');
+            var secondDiv = document.createElement('div');
+            var title = document.createElement('h1');
+            var date = document.createElement('h4');
+            var text = document.createElement('p');
+            var link = document.createElement('a');
             title.textContent = blog.title;
             date.textContent = blog.date;
             text.textContent = blog.description;
             link.href = blog.slug;
             link.textContent = 'To Top';
-
             finalDiv.style.display = 'flex';
             finalDiv.style.flexDirection = 'column';
-
             titleDiv.style.display = 'flex';
             titleDiv.style.justifyContent = 'space-between';
-
             secondDiv.style.display = 'flex';
             secondDiv.style.flexDirection = 'column';
-
             titleDiv.appendChild(title);
             titleDiv.appendChild(date);
-
             secondDiv.appendChild(text);
             secondDiv.appendChild(link);
-
             finalDiv.appendChild(titleDiv);
             finalDiv.appendChild(secondDiv);
-
             container.appendChild(finalDiv);
         });
     }
 }
-
 addBlogToDOM(myList);
